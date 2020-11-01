65cd368b12205eb0c93ff182e8b191e85a429047
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-11-01 19:54:25 +0700

    keep token in cookies and user + company data in localsotrage

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,8 @@
-const BASE_URL = "localhost:8080";
+const BASE_URL = 'http://localhost:8080';
 
+//user
 export const POST_LOGIN = `${BASE_URL}/login`;
+
+
+//company
+export const GET_COMPANY = `${BASE_URL}/companies`
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,16 @@
 import {Form, Button, FormControl, Alert} from 'react-bootstrap'
-import { Link , useHistory} from 'react-router-dom';
-import React, {useState} from 'react';
+import { Link , useHistory} from 'react-router-dom'
+import React, {useState} from 'react'
 
-import {Formik} from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import {Formik} from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import {login, setCompany} from '../../utils/auth'
 
 import Logo from './assetnest-horizontal.svg'
-import './Login.scss';
-import {POST_LOGIN} from '../../constants/urls'
+import {POST_LOGIN, GET_COMPANY} from '../../constants/urls'
 
+import './Login.scss';
 
 
 export default function Login() {
@@ -22,27 +23,36 @@ export default function Login() {
     });
 
     const handleSubmit = (values) => {
-        let config = {
+        const config = {
             headers: {
                 "Content-Type": "application/json"
             }
         }
             
-        let data = {
+        const data = {
             "user": {
                 "email" : values.inputEmail,
                 "password" : values.inputPassword
             }
         }
-
+        
         axios.post(POST_LOGIN, data, config)
         .then(response => {
-            //save cookies storage and local storage
-            console.log(response);
+            //save token to cookies and user to local storage
+            login(response.data)
+
+            //get company data
+            axios.get(GET_COMPANY)
+            .then(response => {
+                console.log(response)
+                setCompany(response.data.data)
+            })
+            
+            //change to dashboard
+            history.push("/dashboard")
         })
         .catch(errors => {
             setShow(true) // show error can't login
-            console.warn(errors);
         });
     }   
 
@@ -60,44 +70,46 @@ export default function Login() {
             values,
             errors,
         })=> (
-            <div className="text-center shadow-lg bg-white py-3 px-4 px-sm-4 login-form d-flex flex-column justify-content-around align-items-center">
+            <div className="login-page">
+                <div className="text-center shadow-lg bg-white py-3 px-4 px-sm-4 login-form d-flex flex-column justify-content-around align-items-center">
             
-            <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
-                <Alert.Heading>Email and Password is invalid!</Alert.Heading>
-            </Alert>
+                    <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
+                        <Alert.Heading>Email and Password is invalid!</Alert.Heading>
+                    </Alert>
 
-            <img src={Logo} alt="logo bambang" className="app-logo mt-md-4"/>
+                    <img src={Logo} alt="logo bambang" className="app-logo mt-md-4"/>
 
-            <Form noValidate onSubmit={handleSubmit} className="mb-md-5">
-                <Form.Group className="position-relative search-field">
-                    <Form.Control
-                    name="inputEmail"
-                    value={values.inputEmail} 
-                    type="email" 
-                    placeholder="Email" 
-                    onChange={handleChange}
-                    isInvalid={!!errors.inputEmail}
-                    />
-                    <FormControl.Feedback type="invalid" tooltip>{errors.inputEmail}</FormControl.Feedback>
-                </Form.Group>
-                <Form.Group className="position-relative">
-                    <Form.Control 
-                    name="inputPassword"
-                    value={values.inputPassword} 
-                    type="password" 
-                    placeholder="Password"
-                    onChange={handleChange}
-                    isInvalid={!!errors.inputPassword}
-                    />
-                    <FormControl.Feedback type="invalid" tooltip>{errors.inputPassword}</FormControl.Feedback>
-                    <Link className="float-right">Forgot Password</Link>
-                </Form.Group>
-                <Button type="submit" className="w-100 mb-3">
-                    Sign In
-                </Button>
-                
-            </Form>
-        </div>
+                    <Form noValidate onSubmit={handleSubmit} className="mb-md-5">
+                        <Form.Group className="position-relative search-field">
+                            <Form.Control
+                            name="inputEmail"
+                            value={values.inputEmail} 
+                            type="email" 
+                            placeholder="Email" 
+                            onChange={handleChange}
+                            isInvalid={!!errors.inputEmail}
+                            />
+                            <FormControl.Feedback type="invalid" tooltip>{errors.inputEmail}</FormControl.Feedback>
+                        </Form.Group>
+                        <Form.Group className="position-relative">
+                            <Form.Control 
+                            name="inputPassword"
+                            value={values.inputPassword} 
+                            type="password" 
+                            placeholder="Password"
+                            onChange={handleChange}
+                            isInvalid={!!errors.inputPassword}
+                            />
+                            <FormControl.Feedback type="invalid" tooltip>{errors.inputPassword}</FormControl.Feedback>
+                            <Link className="float-right" to="/">Forgot Password</Link>
+                        </Form.Group>
+                        <Button type="submit" className="w-100 mb-3">
+                            Sign In
+                        </Button>
+                        
+                    </Form>
+                </div>
+            </div>
         )}
         </Formik>
     );
