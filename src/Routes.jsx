0f3a0f0cb365cd45676801b2a2769bba74876b8e
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-11-07 15:31:50 +0700

    kasi template buar anak anak

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,21 @@
 import Login from "./pages/login/Login";
+import Home from "./pages/home/Home"
 
 export const APP_ROUTE = [
     {
         name : "Login",
-        path : "/",
+        path : "/login",
         exact : true,
         component : Login,
         restricted : true,
         private : false
+    },
+    {
+        name : "Home",
+        path : "/",
+        exact : true,
+        component : Home,
+        restricted : true,
+        private : true
     }
 ]
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            {/* masuksin navbar */}
+            {/* masukin sidebar{
+                    --masukin route sidebar e ndek sini
+                    <Route render={komponen yang udah kalian buat disini}/>
+            } */}
+        </div>
+    )
+}
