611e740f8e0a294e3a0a7508954b9bd14a179132
Nawa <dragneel77@gmail.com>
2020-11-11 21:06:27 +0700

    asset detail ne udah

--- src/components/asset/asset-detail/asset-attachment/AssetAttachment.jsx
+++ src/components/asset/asset-detail/asset-attachment/AssetAttachment.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './AssetAttachment.scss';
+import {BASE_URL} from '../../../../constants/urls';
 
 export default function AssetAttachment(props){
-    // const image = "http://localhost:8000".props.attachment.path;
-    // console.log(image);
     return(
         <div className="d-inline-block text-center mr-3">
-            <img src={"http://localhost:8080" + props.attachment.path} className="rounded mb-1"/>
+            <img src={BASE_URL + props.attachment.path} className="rounded mb-1"/>
             <p>{props.attachment.filename}</p>
         </div>
     );
