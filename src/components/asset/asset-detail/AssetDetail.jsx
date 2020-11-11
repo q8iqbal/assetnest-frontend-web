df01249b0a4684fccf8b3172a47e9ec726a249e1
Nawa <dragneel77@gmail.com>
2020-11-11 18:44:38 +0700

    asset

--- src/components/asset/asset-detail/AssetDetail.jsx
+++ src/components/asset/asset-detail/AssetDetail.jsx
@@ -3,20 +3,25 @@ import './AssetDetail.scss';
 import AssetInfo from './asset-info/AssetInfo';
 import AssetAttachment from './asset-attachment/AssetAttachment';
 import axios from 'axios';
-import {BASE_URL, GET_ASSET } from '../../../constants/urls';
 
 export default function AssetDetail() {
-  const id = 1;
   const [asset, setAsset] = useState({});
   const [attachment, setAttachment] = useState([]);
-  const GET_ATTACHMENT = `${GET_ASSET + id}/attachment`
-  
+
   useEffect(() => {
-    axios.get(GET_ASSET + id)
+    axios.get('http://localhost:8000/assets/2', {
+      headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvbG9naW4iLCJpYXQiOjE2MDUwODczNzQsImV4cCI6MTYwNTA5MDk3NCwibmJmIjoxNjA1MDg3Mzc0LCJqdGkiOiJOVWI4TXJRZ25RWTFrcGRnIiwic3ViIjo1LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-adXxoyGRFc2we95Y98U52WU_N7Lezf5V2ftwjb4nH0'
+      }
+    })
     .then((response) => {
       setAsset(response.data.data);
 
-      axios.get(GET_ATTACHMENT)
+      axios.get('http://localhost:8000/assets/1/attachment',{
+        headers: {
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvbG9naW4iLCJpYXQiOjE2MDUwODczNzQsImV4cCI6MTYwNTA5MDk3NCwibmJmIjoxNjA1MDg3Mzc0LCJqdGkiOiJOVWI4TXJRZ25RWTFrcGRnIiwic3ViIjo1LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-adXxoyGRFc2we95Y98U52WU_N7Lezf5V2ftwjb4nH0'
+        }
+      })
       .then((response) => {
         console.log(response.data.data);
         setAttachment(response.data.data);
@@ -24,13 +29,14 @@ export default function AssetDetail() {
     });
 
   },[]);
-
+  
+  console.log(attachment);
   return (
     <div className="asset-detail row">
         <div className="main col-md-5">
             <h3><span className="text-primary">#</span>{asset.code}</h3>
             <h3>{asset.name}</h3>
-            <img src={BASE_URL + asset.image} className="rounded mb-3 img-fluid w-100"/>
+            <img src={"http://localhost:8000" + asset.image} className="rounded mb-3 img-fluid w-100"/>
             <h4 className="text-primary">Files Attachment</h4>
             {attachment.map(attachment => 
               <AssetAttachment key={attachment.id} attachment={attachment}/>
