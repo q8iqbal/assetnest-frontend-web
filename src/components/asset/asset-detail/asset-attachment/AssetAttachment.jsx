1831532272e38eaa5be0348c22d12e2310da55d6
Nawa <dragneel77@gmail.com>
2020-11-11 18:44:38 +0700

    asset

---
+++ src/components/asset/asset-detail/asset-attachment/AssetAttachment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AssetAttachment.scss';
+
+export default function AssetAttachment(props){
+    // const image = "http://localhost:8000".props.attachment.path;
+    // console.log(image);
+    return(
+        <div className="d-inline-block text-center mr-3">
+            <img src={"http://localhost:8000" + props.attachment.path} className="rounded mb-1"/>
+            <p>{props.attachment.filename}</p>
+        </div>
+    );
+}
