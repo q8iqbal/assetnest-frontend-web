f9f520f2bd348aa18e3060bb97e6c92c8f39644f
Nawa <dragneel77@gmail.com>
2020-11-11 19:35:09 +0700

    coba token

--- src/components/asset/asset-detail/asset-attachment/AssetAttachment.jsx
+++ src/components/asset/asset-detail/asset-attachment/AssetAttachment.jsx
@@ -6,7 +6,7 @@ export default function AssetAttachment(props){
     // console.log(image);
     return(
         <div className="d-inline-block text-center mr-3">
-            <img src={"http://localhost:8000" + props.attachment.path} className="rounded mb-1"/>
+            <img src={"http://localhost:8080" + props.attachment.path} className="rounded mb-1"/>
             <p>{props.attachment.filename}</p>
         </div>
     );
