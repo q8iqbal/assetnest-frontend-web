420084e1cd0e98ff02888e6353a6416380cbdb5b
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-11-11 13:27:46 +0700

    ketika login langsung di arahin ke home

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -48,7 +48,7 @@ export default function Login() {
             })
             
             //change to dashboard
-            history.push("/dashboard")
+            history.push("/home/")
         })
         .catch(errors => {
             setShow(true) // show error can't login
