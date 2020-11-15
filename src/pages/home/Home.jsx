7e9b2376c837cd5d6e5eed40ad4e029b306960e3
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-11-15 15:33:17 +0700

    ehmm... entah lupa

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import PrivateRoute from '../../components/PrivateRoute'
 import Error from "../../components/404/Error"
 import { getUser, getCompany } from "../../utils/auth"
 import { BASE_URL } from "../../constants/urls";
+import ProfileImage from '../../assets/icons/profile.png'
 
 
 
@@ -13,9 +14,11 @@ export default function Home() {
     const userData = getUser()
     const companyData = getCompany()
 
+    const image = userData.image === ''? ProfileImage : BASE_URL+userData.image
+
     return (
         <>
-            <Sidebar route={USER_ROUTE} userName={userData.name} userImage={BASE_URL+userData.image} companyName={companyData.name}>
+            <Sidebar route={USER_ROUTE} userName={userData.name} userImage={image} companyName={companyData.name}>
                 <Switch>
                     {
                         USER_ROUTE.map( value => {
