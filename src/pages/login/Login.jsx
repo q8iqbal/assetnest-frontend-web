8bcfaf0712ac302ba2ef53cfd9d3f33c1c740fc6
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2021-01-17 14:32:44 +0700

    seng sabar ae

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
-import {Form, Button, FormControl, Alert} from 'react-bootstrap'
+import {Form, Button, FormControl, Alert, Modal} from 'react-bootstrap'
 import { Link , useHistory} from 'react-router-dom'
 import React, {useState} from 'react'
+import { AiOutlineInfoCircle,AiFillQuestionCircle } from "react-icons/ai";
+import Aou from "../../components/about_us/Aou"
 
 import {Formik} from 'formik'
 import * as Yup from 'yup'
@@ -12,10 +14,18 @@ import {POST_LOGIN, GET_COMPANY} from '../../constants/urls'
 
 import './Login.scss';
 
+import anButton from './an.png'
+import wiButton from './wi.png'
+
 
 export default function Login() {
     const [show, setShow] = useState(false);
     const history = useHistory();
+
+    // modal about us
+    const [showAou, setShowAou] = useState(false);
+    const handleClose = () => setShowAou(false);
+    const handleShow = () => setShowAou(true);
 
     const schema = Yup.object({
         inputEmail : Yup.string().email('invalid email').required('Required'),
@@ -39,16 +49,19 @@ export default function Login() {
         axios.post(POST_LOGIN, data, config)
         .then(response => {
             //save token to cookies and user to local storage
-            login(response.data)
-
-            //get company data
-            axios.get(GET_COMPANY,{headers : {'Authorization' : 'Bearer'+response.data.token}})
-            .then(response => {
-                setCompany(response.data.data)
-                //change to dashboard
-                history.push("/home/")
-            })
-            
+            const role = response.data.user.role
+            if(role === "user"){
+                setShow(true)
+            }else{
+                login(response.data)
+                //get company data
+                axios.get(GET_COMPANY,{headers : {'Authorization' : 'Bearer'+response.data.token}})
+                .then(response => {
+                    setCompany(response.data.data)
+                    //change to dashboard
+                    history.push("/home/")
+                })
+            }
         })
         .catch(errors => {
             setShow(true) // show error can't login
@@ -69,16 +82,14 @@ export default function Login() {
             values,
             errors,
         })=> (
+            <>
             <div className="login-page">
                 <div className="text-center shadow-lg bg-white py-3 px-4 px-sm-4 login-form d-flex flex-column justify-content-around align-items-center">
-            
                     <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
                         <Alert.Heading>Email and Password is invalid!</Alert.Heading>
                     </Alert>
-
                     <img src={Logo} alt="logo bambang" className="app-logo mt-md-5"/>
-
-                    <Form noValidate onSubmit={handleSubmit} className="mb-md-5">
+                    <Form noValidate onSubmit={handleSubmit}>
                         <Form.Group className="position-relative search-field">
                             <Form.Control
                             name="inputEmail"
@@ -104,10 +115,31 @@ export default function Login() {
                         <Button type="submit" className="w-100 mb-3">
                             Sign In
                         </Button>
-                        
                     </Form>
+                    <span className="mb-md-5"> 
+                        <a href=""> <img src={anButton} alt="" loading="lazy" width="35%" height="auto"/> 
+                        </a> 
+                        <a href=""> <img src={wiButton} alt="" loading="lazy"  width="35%" height="auto"/> </a>
+                    </span>
                 </div>
+                <span className="test">
+                    <Button className="aou" onClick={handleShow}>
+                        <AiOutlineInfoCircle/> <span className="hide">About us</span>
+                    </Button>
+                    <Button className="aou">
+                        <AiFillQuestionCircle/> <span className="hide">How to use</span>
+                    </Button>
+                </span>
             </div>
+
+
+            <Modal show={showAou} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>About Us</Modal.Title>
+                </Modal.Header>
+                <Modal.Body><Aou/></Modal.Body>
+            </Modal>
+            </>
         )}
         </Formik>
     );
